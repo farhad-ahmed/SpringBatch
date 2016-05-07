af299beb86b3ece022f8ec34a1b71cdd075ac8d6
Prottoy <cloud_no_9@msn.com>
2016-05-07 03:08:24 -0400

    Scheduler

--- src/main/java/farhad/spring/batch/SpringBatch/App.java
+++ src/main/java/farhad/spring/batch/SpringBatch/App.java
@@ -1,13 +1,20 @@
 package farhad.spring.batch.SpringBatch;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@EnableScheduling
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
     }
 }
