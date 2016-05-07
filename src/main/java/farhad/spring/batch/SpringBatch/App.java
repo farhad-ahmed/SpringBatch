08bbed7669fe4da1c6cdaf7b83c0fcfbdb520efb
Prottoy <cloud_no_9@msn.com>
2016-05-07 02:05:36 -0400

    commited now

---
+++ src/main/java/farhad/spring/batch/SpringBatch/App.java
@@ -0,0 +1,13 @@
+package farhad.spring.batch.SpringBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
